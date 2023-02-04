758eba4d16a615331441f63597888f3fec11d761
RajParmar03 <rjbusiness03@gmail.com>
2023-02-04 18:48:12 +0530

    added the data to the database and also create the API for the products to search according to the filter.

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,17 @@
 const express = require("express"); // importing express from the packages.
+const ProductModel = require("../models/product.model");
 
 const productRouter = express.Router(); // creating the router for the product.
 
 
 // this is the get request for the product.
-productRouter.get("/" , (req , res) => {
-    res.status(400).send("You are on the product router and get request of the raj parmar website..");
+productRouter.get("/" , async (req , res) => {
+    try {
+        let data = await ProductModel.find(req.query);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(400).send({error : error});
+    }
 });
 
 
