540b9ade2774235149e3855fd3ff738d5f922f1d
RajParmar03 <rjbusiness03@gmail.com>
2023-02-08 15:45:32 +0530

    completed the form validation and also added the user page and get user route in the userRouter.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import React, { useRef } from 'react';
+import { Box, FormLabel, Heading, Input } from '@chakra-ui/react';
+import axios from 'axios';
+
 
 
 const Login = () => {
+
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      let loginObj = {
+        email : emailInput.current.value,
+        password : passwordInput.current.value
+      }
+      let ans = await axios.post("http://localhost:1010/user/login" , loginObj);
+      alert(ans.data.message);
+      localStorage.setItem("token",ans.data.token);
+    } catch (error) {
+      alert(error.message);
+    }
+    
+
+  }
+
+
   return (
-    <Box marginTop={10}>
-      <Heading as='h1' size='md' noOfLines={1}>
-        Login
-      </Heading>
+    <>
+    <Heading as={"h1"} size={"2xl"} color="orange">Login</Heading>
+    <Box m={"40px auto auto auto"} w={"30%"}>
+      <form onSubmit={(e) => handleLogin(e)}>
+        <FormLabel>Email / User Name / Mobile Number</FormLabel>
+        <Input type='text' placeholder='Enter Your Email or User Name or Mobile Number...' border={"1px"} ref={emailInput}/>
+        <FormLabel>Password</FormLabel>
+        <Input type='password' placeholder='Enter Your Password...' border={"1px"} ref={passwordInput}/>
+        <Input m={"20px auto auto auto"} type="submit" value="Login" border={"2px solid orange"} fontSize={"20px"} color={"orange.400"} fontWeight={"bold"}  />
+      </form>
     </Box>
+    </>
   )
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,30 +1,39 @@
 import React, { useRef } from 'react';
-import { Box,  FormLabel, Input } from '@chakra-ui/react';
+import { Box, Divider, Flex, FormLabel, Heading, Input, Select } from '@chakra-ui/react';
 import axios from 'axios';
+import image from "../userImage.png";
 
 
 const Signup = () => {
 
+  const roleInput = useRef(null);
   const nameInput = useRef(null);
   const emailInput = useRef(null);
   const userNameInput = useRef(null);
   const passwordInput = useRef(null);
   const confirmPasswordInput = useRef(null);
   const mobileInput = useRef(null);
+  const imageInput = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(passwordInput.current.value === confirmPasswordInput.current.value){
-      const userObject = {
-        name : nameInput.current.value,
-        username : userNameInput.current.value,
-        email : emailInput.current.value,
-        phone : mobileInput.current.value,
-        password : passwordInput.current.value,
-      };
-      let ans = await axios.post("http://localhost:1010/user/signup" , userObject);
-      console.log(ans);
-    }else{
+    if (passwordInput.current.value === confirmPasswordInput.current.value) {
+      if(nameInput.current.value && userNameInput.current.value && emailInput.current.value && mobileInput.current.value && passwordInput.current.value){
+        const userObject = {
+          image : imageInput.current.value || image,
+          name: nameInput.current.value,
+          username: userNameInput.current.value,
+          email: emailInput.current.value,
+          phone: mobileInput.current.value,
+          password: passwordInput.current.value,
+          role : roleInput.current.value || "buyer"
+        };
+        let ans = await axios.post("http://localhost:1010/user/signup", userObject);
+        alert(ans.data.message);
+      }else{
+        alert("please enter all the filled...");
+      }
+    } else {
       alert("Enter the password properly...");
     }
 
@@ -32,23 +41,38 @@ const Signup = () => {
 
 
   return (
-    <Box m={"20px auto auto auto"} w={"30%"}>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <FormLabel>Name</FormLabel>
-        <Input type='text' placeholder='Enter Your Full Name...' border={"1px"} ref={nameInput}/>
-        <FormLabel>Email address</FormLabel>
-        <Input type='email' placeholder='Enter Your Email...' border={"1px"} ref={emailInput}/>
-        <FormLabel>User Name</FormLabel>
-        <Input type='text' placeholder='Create New User Name...' border={"1px"} ref={userNameInput}/>
-        <FormLabel>Mobile Number</FormLabel>
-        <Input type='number' placeholder='Enter Your Mobile Number...' border={"1px"} ref={mobileInput}/>
-        <FormLabel>Password</FormLabel>
-        <Input type='password' placeholder='Create A Strong Password...' border={"1px"} ref={passwordInput}/>
-        <FormLabel>Confirm Password</FormLabel>
-        <Input type='password' placeholder='Enter The Same Password Again... ' border={"1px"} ref={confirmPasswordInput}/>
-        <Input m={"20px auto auto auto"} type="submit" border={"2px solid orange"} fontSize={"20px"} color={"orange.400"} fontWeight={"bold"}  />
-      </form>
-    </Box>
+    <>
+      <Heading as={"h1"} size={"2xl"} color="orange" m={"auto auto 30px auto"}>Create A New Account</Heading>
+      <Divider />
+      <Box m={"30px auto auto auto"} w={"30%"}>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <Flex justifyContent={"flex-end"}>
+            <Box w={"40%"}>
+              <FormLabel color={"orange"}>Select Role</FormLabel>
+              <Select placeholder='Select role' ref={roleInput} border={"2px solid orange"}>
+                <option value='seller'>Want to Start Selling My Products</option>
+                <option value='buyer'>Want to Buy Some Products</option>
+              </Select>
+            </Box>
+          </Flex>
+          <FormLabel>Name</FormLabel>
+          <Input type='text' placeholder='Enter Your Full Name...' border={"1px"} ref={nameInput} />
+          <FormLabel>Email address</FormLabel>
+          <Input type='email' placeholder='Enter Your Email...' border={"1px"} ref={emailInput} />
+          <FormLabel>User Name</FormLabel>
+          <Input type='text' placeholder='Create New User Name...' border={"1px"} ref={userNameInput} />
+          <FormLabel>Mobile Number</FormLabel>
+          <Input type='number' placeholder='Enter Your Mobile Number...' border={"1px"} ref={mobileInput} />
+          <FormLabel>Image Url</FormLabel>
+          <Input type='number' placeholder='Enter Your Image Url...' border={"1px"} ref={imageInput} />
+          <FormLabel>Password</FormLabel>
+          <Input type='password' placeholder='Create A Strong Password...' border={"1px"} ref={passwordInput} />
+          <FormLabel>Confirm Password</FormLabel>
+          <Input type='password' placeholder='Enter The Same Password Again... ' border={"1px"} ref={confirmPasswordInput} />
+          <Input m={"20px auto auto auto"} type="submit" value="Sign-Up" border={"2px solid orange"} fontSize={"20px"} color={"orange.400"} fontWeight={"bold"} />
+        </form>
+      </Box>
+    </>
   )
 }
 
