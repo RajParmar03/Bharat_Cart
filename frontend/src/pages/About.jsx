c71a340721cd9261d86912cb738a132cac3d1fc6
RajParmar03 <rjbusiness03@gmail.com>
2023-02-17 21:12:30 +0530

    added redux in the frontend application for managing the loading and error state and also implement this state management in selectproduct page and in navbar.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,23 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Button, Heading } from '@chakra-ui/react';
 import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startError, startLoading, stopError, stopLoading } from '../Redux/stateManager/stateManager.action';
 
 const About = () => {
+
+  const store = useSelector(store => store);
+
+  const dispatch = useDispatch();
+
+  console.log(store);
   return (
     <Box m={"130px auto 30px auto"}>
       <Heading as='h1' size='md' noOfLines={1}>
-        About
+        <Button onClick={() => dispatch(startLoading())}>start loading</Button>
+        <Button onClick={() => dispatch(stopLoading())}>stop loading</Button>
+        <Button onClick={() => dispatch(startError())}>start error</Button>
+        <Button onClick={() => dispatch(stopError())}>stop error</Button>
       </Heading>
     </Box>
   )
--- frontend/src/pages/SelectProducts.jsx
+++ frontend/src/pages/SelectProducts.jsx
@@ -1,8 +1,11 @@
-import { Box, Button, Divider, Flex, Grid, Heading, Input, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex, Grid, Heading, Input, Spinner, Text, VStack } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useRef, useState } from 'react'
 import { useEffect } from 'react';
+import { GoSearch } from 'react-icons/go';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
+import { startError, startLoading, stopLoading } from '../Redux/stateManager/stateManager.action';
 
 let baseUrl = process.env.REACT_APP_BASEURL;
 
@@ -19,6 +22,11 @@ const SelectProducts = () => {
     const [data, setData] = useState([]);
     const ID = useRef(null);
 
+    const store = useSelector(store => store);
+    const dispatch = useDispatch();
+
+
+
     const handleCategory = (category) => {
         navigate(`/products/${category}`);
     }
@@ -28,11 +36,19 @@ const SelectProducts = () => {
             clearTimeout(ID.current);
         }
         if (val) {
+            dispatch(startLoading());
             ID.current = setTimeout(() => {
                 getData(val).then((res) => {
                     setData(res);
+                    dispatch(stopLoading());
+                }).catch((error) => {
+                    dispatch(startError);
                 });
             }, 1500);
+        } else {
+            setData([]);
+            dispatch(stopLoading());
+
         }
     }, [val]);
     // console.log(data);
@@ -44,47 +60,78 @@ const SelectProducts = () => {
 
     return (
         <>
+
+
+
             <Flex justifyContent={"space-around"} alignItems={"center"} m={"130px auto 30px auto"} w={"60%"}>
                 <Input onChange={(e) => handleChange(e.target.value)} w={"80%"} variant='filled' size='lg' placeholder='Search product here...'></Input>
+                {
+                    store.isLoading ?
+                        <Spinner size='sm' style={{
+                            position: "relative",
+                            right: "100px",
+                        }} />
+                        :
+                        <Button style={{
+                            position: "relative",
+                            right: "100px",
+                        }} variant={"unstyled"}><GoSearch size={"20px"} /></Button>
+                }
             </Flex>
             <Divider />
             {
-                val ? <Grid templateColumns='repeat(5, 1fr)' gap={6} m={10} >
-                    {
-                        data.map((elem) => {
-                            return (
-                                <VStack key={elem.title + elem.price} boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px" p={5}>
-                                    <Box h={"50px"}>
-                                        <img src={"/bagImage.jpg"} alt={elem.title} style={{ height: "100%" }} />
-                                    </Box>
-                                    <Heading as="h3" size='md'>Title :- {elem.title}</Heading>
-                                    <Text textDecoration="line-through">Value :- {elem.strike}</Text>
-                                    <Text>Price :- {elem.price}</Text>
-                                    <Text>Discount :- {elem.discount}</Text>
-                                    <Text>Main-Category :- {elem.main_category}</Text>
-                                    <Text>Sub-Category :- {elem.sub_category}</Text>
-                                    <Button>Add to Cart</Button>
-                                </VStack>
-                            )
-                        })
-                    }
-                </Grid>
+                store.isLoading ?
+                    <Spinner
+                        thickness='5px'
+                        speed='0.5s'
+                        emptyColor='gray.200'
+                        color='blue.500'
+                        size='xl'
+                    />
                     :
-                    <Box m={"60px auto auto auto"} w={"70%"} h={"300px"}>
-                        <Heading as={"h1"} size="xl" textAlign={"left"} m={"auto auto 30px 100px"}>Search Products via Category...</Heading>
-                        <Grid templateColumns='repeat(3, 1fr)' m={"auto"} p={"10px"} w={"70%"} gap={"50px"} h={"80%"}>
-                            <Flex onClick={() => handleCategory("clothing")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Clothing</Flex>
-                            <Flex onClick={() => handleCategory("Educational")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"> Educational
-                            </Flex>
-                            <Flex onClick={() => handleCategory("Footwear")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Footwear
-                            </Flex>
-                            <Flex onClick={() => handleCategory("Gadgets")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Gadgets
-                            </Flex>
-                            <Flex onClick={() => handleCategory("Electronics")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Electronics</Flex>
-                        </Grid>
+                    <Box>
+
+                        {
+                            data.length ? <Grid templateColumns='repeat(5, 1fr)' gap={6} m={10} >
+                                {
+                                    data.map((elem) => {
+                                        return (
+                                            <VStack key={elem.title + elem.price} boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px" p={5}>
+                                                <Box h={"50px"}>
+                                                    <img src={"/bagImage.jpg"} alt={elem.title} style={{ height: "100%" }} />
+                                                </Box>
+                                                <Heading as="h3" size='md'>Title :- {elem.title}</Heading>
+                                                <Text textDecoration="line-through">Value :- {elem.strike}</Text>
+                                                <Text>Price :- {elem.price}</Text>
+                                                <Text>Discount :- {elem.discount}</Text>
+                                                <Text>Main-Category :- {elem.main_category}</Text>
+                                                <Text>Sub-Category :- {elem.sub_category}</Text>
+                                                <Button>Add to Cart</Button>
+                                            </VStack>
+                                        )
+                                    })
+                                }
+                            </Grid>
+                                :
+                                <Box m={"60px auto auto auto"} w={"70%"} h={"300px"}>
+                                    <Heading as={"h1"} size="xl" textAlign={"left"} m={"auto auto 30px 100px"}>Search Products via Category...</Heading>
+                                    <Grid templateColumns='repeat(3, 1fr)' m={"auto"} p={"10px"} w={"70%"} gap={"50px"} h={"80%"}>
+                                        <Flex onClick={() => handleCategory("clothing")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Clothing</Flex>
+                                        <Flex onClick={() => handleCategory("Educational")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"> Educational
+                                        </Flex>
+                                        <Flex onClick={() => handleCategory("Footwear")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Footwear
+                                        </Flex>
+                                        <Flex onClick={() => handleCategory("Gadgets")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Gadgets
+                                        </Flex>
+                                        <Flex onClick={() => handleCategory("Electronics")} borderRadius={"20px"} border={"7px solid orange"} fontSize={"25px"} fontWeight={"bold"} textAlign={"centre"} justifyContent={"center"} alignItems={"center"} h={"100%"} boxShadow="rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset">Electronics</Flex>
+                                    </Grid>
+                                </Box>
+                        }
                     </Box>
             }
         </>
+
+
     )
 }
 
